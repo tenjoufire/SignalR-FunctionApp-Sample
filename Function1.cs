dda8bbdc14f779eaae094f342b6e961754d56d17
Kaisei Tsujimoto <tenjoufire7@gmail.com>
2020-06-02 10:39:37 +0900

    プロジェクト ファイルを追加します。

---
+++ Function1.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.AspNetCore.Http;
+
+namespace katsujim_SignalR_functionApp
+{
+    public static class Function1
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "chat")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("messages")]
+        public static Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] object message,
+            [SignalR(HubName = "chat")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "newMessage",
+                    Arguments = new[] { message }
+                });
+        }
+    }
+}
